99a2a91935f436870aa24cdd2c2293c98949146c
Arlan Linux <marinhomoraisster@gmail.com>
2021-03-19 11:10:19 -0300

    agendak - 01 - Criando o primeiro App

---
+++ app/src/main/java/br/com/alura/agendak/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.alura.agendak
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
+    }
+}
