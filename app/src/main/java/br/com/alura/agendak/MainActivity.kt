4bf5df9644b8998f21788cf173bf1c491b6fe433
Arlan Linux <marinhomoraisster@gmail.com>
2021-03-19 14:43:34 -0300

    agendak_1 - 02 - Iniciando a implementação de lista

--- app/src/main/java/br/com/alura/agendak/MainActivity.kt
+++ app/src/main/java/br/com/alura/agendak/MainActivity.kt
@@ -3,10 +3,15 @@ package br.com.alura.agendak
 import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val alunos = listOf<String>("Alex","Fran","José")
+        textView1.text = alunos[0]
+        textView2.text = alunos[1]
+        textView3.text = alunos[2]
     }
 }
