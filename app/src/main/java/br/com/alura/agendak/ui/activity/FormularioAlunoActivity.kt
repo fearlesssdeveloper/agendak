580838c2d43bb89125a502e63d8f04e789387f1b
Arlan Linux <marinhomoraisster@gmail.com>
2021-03-23 15:50:01 -0300

    agendak_1 - 05 - Refatoração de código

--- app/src/main/java/br/com/alura/agendak/ui/activity/FormularioAlunoActivity.kt
+++ app/src/main/java/br/com/alura/agendak/ui/activity/FormularioAlunoActivity.kt
@@ -1,33 +1,52 @@
 package br.com.alura.agendak.ui.activity
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.agendak.R
 import br.com.alura.agendak.dao.AlunoDAO
 import br.com.alura.agendak.model.Aluno
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_formulario_aluno.*
 
 class FormularioAlunoActivity : AppCompatActivity() {
+    private lateinit var campoNome: TextInputEditText
+    private lateinit var campoTelefone: TextInputEditText
+    private lateinit var campoEmail: TextInputEditText
+    private val dao = AlunoDAO()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_aluno)
 
-        val dao = AlunoDAO()
+        title = "Novo aluno"
 
-        val campoNome = activity_formulario_aluno_nome
-        val campoTelefone = activity_formulario_aluno_telefone
-        val campoEmail = activity_formulario_aluno_email
+        inicializacaoDosCampos()
+        configuraBotaoSalvar()
+    }
 
+    private fun configuraBotaoSalvar() {
         activity_formulario_aluno_button.setOnClickListener {
-            var nome = campoNome.text.toString().trim()
-            var telefone = campoTelefone.text.toString().trim()
-            var email = campoEmail.text.toString().trim()
-
-            val alunoCriado = Aluno(nome, telefone, email)
-            dao.salva(alunoCriado)
-            finish()
+            val alunoCriado = criaAluno()
+            salva(alunoCriado)
         }
+    }
+
+    private fun inicializacaoDosCampos() {
+        campoNome = activity_formulario_aluno_nome
+        campoTelefone = activity_formulario_aluno_telefone
+        campoEmail = activity_formulario_aluno_email
+    }
+
+    private fun salva(aluno: Aluno) {
+        dao.salva(aluno)
+        finish()
+    }
+
+    private fun criaAluno(): Aluno {
+        val nome = campoNome.text.toString().trim()
+        val telefone = campoTelefone.text.toString().trim()
+        val email = campoEmail.text.toString().trim()
+
+        return Aluno(nome, telefone, email)
     }
 }
--- app/src/main/java/br/com/alura/agendak/ui/activity/ListaAlunosActivity.kt
+++ app/src/main/java/br/com/alura/agendak/ui/activity/ListaAlunosActivity.kt
@@ -9,19 +9,33 @@ import br.com.alura.agendak.dao.AlunoDAO
 import kotlinx.android.synthetic.main.activity_lista_alunos.*
 
 class ListaAlunosActivity : AppCompatActivity() {
+    private val dao = AlunoDAO()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_alunos)
 
         title = "Lista de Alunos"
+        configuraFabNovoAluno()
+    }
+
+    private fun configuraFabNovoAluno() {
         activity_lista_alunos_fab_novo_aluno.setOnClickListener {
-            startActivity(Intent(this, FormularioAlunoActivity::class.java))
+            abreFormularioAlunoActivity()
         }
+    }
+
+    private fun abreFormularioAlunoActivity() {
+        startActivity(Intent(this, FormularioAlunoActivity::class.java))
     }
 
     override fun onResume() {
         super.onResume()
-        val dao = AlunoDAO()
-        activity_lista_alunos_listview.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dao.alunos)
+        configuraLista()
+    }
+
+    private fun configuraLista() {
+        activity_lista_alunos_listview.adapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, dao.alunos)
     }
 }
