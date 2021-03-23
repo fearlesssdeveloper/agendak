143c661f7ca9999a374e574fa976ec4df42dbbf4
Arlan Linux <marinhomoraisster@gmail.com>
2021-03-23 10:31:08 -0300

    agendak_1 - 04 - Inserindo ícone e App Bar

--- app/src/main/java/br/com/alura/agendak/MainActivity.kt
+++ app/src/main/java/br/com/alura/agendak/MainActivity.kt
@@ -1,15 +1,15 @@
 package br.com.alura.agendak
 
-import android.app.Activity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Lista de Alunos"
         val alunos = listOf<String>("Alex","Fran","José")
         activity_main_lista_de_alunos.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, alunos)
 
